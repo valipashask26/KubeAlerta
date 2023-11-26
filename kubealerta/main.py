cd7d824265d06a60b903821a30754fa0edf4c281
Sk Valipasha <sk.valipasha@aquasec.com>
2023-11-26 17:48:33 +0530

    updated manifest

--- kubealerta/main.py
+++ kubealerta/main.py
@@ -2,14 +2,19 @@
 from kubealerta.config.config import load_kube_config
 from kubealerta.operator.nodenotify import get_filtered_resources
 from kubernetes.client.rest import ApiException
+import time
 
 def main():
     try:
         # Load Kubernetes configuration from config.py
         load_kube_config()
 
-        # Call the receiver function without specifying a namespace
-        get_filtered_resources()
+        while True:
+            # Call the receiver function without specifying a namespace
+            get_filtered_resources()
+
+            # Sleep for 30 seconds
+            time.sleep(30)
 
     except ApiException as e:
         print(f"Error during execution: {e}")
